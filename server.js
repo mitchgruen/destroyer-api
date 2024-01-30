50da6c807bae60b276a3e81c1033383d456a7b0d
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-01-29 20:37:21 -0500

    add test route

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 8000
-const cors = require('cors')
-app.use(cors())
-app.use(express.json())
+const express = require('express');
+const app = express();
+const port = 8000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Server is running on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/test', (req, res) => res.send('test'));
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
