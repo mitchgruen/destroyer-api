2c9100c564418a9a9c3e29f587965315cde9e987
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2024-04-10 12:57:36 -0400

    Change port to 80 for production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 80;
 const cors = require('cors');
 const errorHandler = require('./middleware/error.js');
 require('dotenv').config();
